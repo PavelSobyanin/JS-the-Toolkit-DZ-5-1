25410a208e901d03ef32025a2f9613cb78fdf190
Pavel Sobyanin <pasha_ne1@email.com>
2024-02-27 22:42:26 +0300

    adjustments

---
+++ src/js/__test__/daemon.test.js
@@ -0,0 +1,13 @@
+import Daemon from '../daemon.js';
+
+test('Daemon', () => {
+    const daemon = new Daemon('Daemy');
+    expect(daemon).toEqual({
+        name: 'Daemy',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        attack: 10,
+        defence: 40,
+    });
+});
